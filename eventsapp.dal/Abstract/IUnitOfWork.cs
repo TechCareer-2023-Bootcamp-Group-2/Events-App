8d04b7116816414eee45d1ab07bc6f620d0dc148
Enes Gündüz <enesg1997@gmail.com>
2023-11-23 16:22:42 +0300

    Add: UnitOfWork

---
+++ eventsapp.dal/Abstract/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+namespace eventsapp.dal.Abstract
+{
+    public interface IUnitOfWork
+    {
+        IEventsRepository EventsRepo{get;}
+        IEventTypesRepitory EventTypesRepo{get;}
+        void Save();
+    }
+}
---
+++ eventsapp.dal/Concrete/UnitOfWork.cs
@@ -0,0 +1,22 @@
+using eventsapp.dal.Abstract;
+using eventsapp.dal.Data;
+
+namespace eventsapp.dal.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly EventsDBContext _ctx;
+        public UnitOfWork(EventsDBContext ctx)=>_ctx=ctx;
+
+        private EventsRepository _eventsRepository;
+        private EventTypesRepository _eventTypesRepository;
+
+        public IEventsRepository EventsRepo=>_eventsRepository=_eventsRepository??new EventsRepository(_ctx);
+        public IEventTypesRepitory EventTypesRepo=>_eventTypesRepository=_eventTypesRepository??new EventTypesRepository(_ctx);
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
